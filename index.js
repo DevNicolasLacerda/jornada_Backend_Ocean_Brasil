70d22a9bc1e1dd082244340f89899ae71d2757c6
DevNicolasLacerda <nicklacerda2018@gmail.com>
2023-06-13 11:28:50 -0300

    Mongo DB Instalado

--- index.js
+++ index.js
@@ -1,61 +1,77 @@
+const { MongoClient } = require('mongodb');
 const express = require("express");
-const app = express();
 
-// Indicamos que o Express deve considerar o Body
-// das requisições como JSON
-app.use(express.json());
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
 
-app.get("/" , function (req, res) {
-  res.send("Hello World");
-});
+// DataBase Name
+const dbName = 'jornada_backend_13062023';
 
-app.get("/oi", function (req, res) {
-  res.send("Olá, Mundo!");
-});
+async function main() {
 
-const herois = ["Mulher maravilha", "Homem de ferro", "Homem Formiga"];
+  const app = express();
 
-app.get("/herois", function (req, res) {
-  res.send(herois.filter(Boolean));
-});
+  // Indicamos que o Express deve considerar o Body
+  // das requisições como JSON
+  app.use(express.json());
 
-app.post("/herois", function (req, res) {
-  // console.log(req.body, typeof req.body)
+  app.get("/" , function (req, res) {
+    res.send("Hello World");
+  });
 
-  const nome = req.body.nome;
-  //console.log(nome, typeof nome);
+  app.get("/oi", function (req, res) {
+    res.send("Olá, Mundo!");
+  });
 
-  herois.push(nome);
+   const herois = ["Mulher maravilha", "Homem de ferro", "Homem Formiga"];
 
-  res.send("Item criado com sucesso!")
-});
+  app.get("/herois", function (req, res) {
+    res.send(herois.filter(Boolean));
+  });
 
-app.get("/herois/:id", function (req, res) {
-  const id = req.params.id;
+  app.post("/herois", function (req, res) {
+    // console.log(req.body, typeof req.body)
+
+    const nome = req.body.nome;
+    //console.log(nome, typeof nome);
+
+    herois.push(nome);
+
+    res.send("Item criado com sucesso!")
+  });
+
+  app.get("/herois/:id", function (req, res) {
+    const id = req.params.id;
   
-  const item = herois[id - 1];
+    const item = herois[id - 1];
 
-  res.send(item);
-});
+    res.send(item);
+  });
 
-app.put("/herois/:id", function (req, res) {
-  const id = req.params.id;
+  app.put("/herois/:id", function (req, res) {
+    const id = req.params.id;
 
-  const novoNome = req.body.nome;
+    const novoNome = req.body.nome;
   
-  herois[id - 1] = novoNome;
+    herois[id - 1] = novoNome;
 
-  res.send("Item atualizado com sucesso!");
-});
+    res.send("Item atualizado com sucesso!");
+  });
 
-app.delete("/herois/:id", function (req, res) {
-  const id = req.params.id;
+  app.delete("/herois/:id", function (req, res) {
+    const id = req.params.id;
 
-  delete herois[id - 1];
+    delete herois[id - 1];
 
-  res.send("Item deletado com sucesso!");
-});
+    res.send("Item deletado com sucesso!");
+  });
 
-app.listen(3000, function () {
-  console.log("Servidor rodando em http://localhost:3000")
-});
+  app.listen(3000, function () {
+    console.log("Servidor rodando em http://localhost:3000")
+  });
+}
+
+main()
+  .catch(console.error)
+  .finally(() => client.close());
